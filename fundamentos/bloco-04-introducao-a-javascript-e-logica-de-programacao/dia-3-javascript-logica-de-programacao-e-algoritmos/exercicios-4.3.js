16664d2871803bea9b01daf146ea0b3d8e329d30
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-07 11:04:22 -0300

    quadrado de asteriscos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-4.3.js
@@ -0,0 +1,16 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+
+let quadrado = " ";
+let asteriscos = "*";
+let n = 5;
+
+for (let index = 0; index < n; index += 1) {
+    quadrado += asteriscos ;
+    
+}
+for (let index = 0; index < n; index += 1) {
+    console.log(quadrado)
+}
+
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
