c9de81126aafa1a4c763e2987784efed988c048e
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-20 21:06:36 -0300

    exercicio 7

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -24,21 +24,15 @@ function criandoCalendario() {
     for (let i = 0; i < dezDaysList.length; i += 1) {
         let dia = dezDaysList[i];
         let criaDia = document.createElement("li");
+        calendario.appendChild(criaDia);
+        criaDia.innerText = dia;
         if (dia === 25){
-            calendario.appendChild(criaDia);
-            criaDia.innerText = dia;
             criaDia.className = "day holiday friday";
         } else if (dia === 24 || dia === 31) {
-            calendario.appendChild(criaDia);
-            criaDia.innerText = dia;
             criaDia.className = "day holiday";
         } else if (dia === 4 || dia === 11 || dia === 18) {
-            calendario.appendChild(criaDia);
-            criaDia.innerText = dia;
             criaDia.className = "day friday";            
-        } else {
-            calendario.appendChild(criaDia);
-            criaDia.innerText = dia;
+        } else {            
             criaDia.className = "day";            
         }
     }
@@ -51,6 +45,86 @@ function criaFeriados(Feriados) {
     botao.id = "btn-holiday";
     let lugarDoBotao = document.querySelector(".buttons-container");
     lugarDoBotao.appendChild(botao);
-    botao.innerText = "Feriados"
+    botao.innerText = "Feriados";
 }
 criaFeriados();
+
+//  Exercício 3:
+let botao = document.getElementById("btn-holiday");
+
+function mudaCor(event) {
+    let botao = document.getElementById("btn-holiday");
+    let feriados = document.getElementsByClassName("holiday");
+    for (i = 0; i < feriados.length; i += 1) { 
+    if (feriados[i].style.backgroundColor === "red") {
+        feriados[i].style.backgroundColor = "rgb(238,238,238)"
+    } else {
+        feriados[i].style.backgroundColor = "red";
+    }
+} 
+}
+botao.addEventListener("click", mudaCor);
+
+//  Exercício 4:
+
+function buttonFriday(nomeDoBotao) {
+    let localDoBotao = document.querySelector(".buttons-container");
+    let botao = document.createElement("button");
+    localDoBotao.appendChild(botao);
+    botao.id = "btn-friday";
+    botao.innerHTML = nomeDoBotao;
+}
+buttonFriday("Sexta-feira");
+
+//  Exercício 5:
+
+function sextaFeira(todasAsSextasFeira) {
+    let botao = document.getElementById("btn-friday");
+    let diaSexta = document.getElementsByClassName("friday");
+    let sextaPorExtenso = "Sexta";
+
+    botao.addEventListener("click", function() {
+        for (let i = 0; i < diaSexta.length; i += 1) {
+            if (diaSexta[i].innerHTML !== sextaPorExtenso) {
+                diaSexta[i].innerHTML = sextaPorExtenso;
+            } else {
+                diaSexta[i].innerHTML = todasAsSextasFeira[i];
+            }
+        }
+    })
+}
+let fridays = [4, 11, 18, 25];
+sextaFeira(fridays)
+
+//  Exercício 6:
+function colocaMouseEmCima() {
+    let dias = document.getElementById("days");
+
+    dias.addEventListener("mouseover", function(event) {
+        event.target.style.fontSize = "30px";
+        event.target.style.fontWeight = "600";
+    })
+}
+
+function tiraMouseDeCima() {
+    let dias = document.getElementById("days");
+
+    dias.addEventListener("mouseout", function(event) {
+        event.target.style.fontSize = "20px";
+        event.target.style.fontWeight = "200";
+    })
+}
+
+colocaMouseEmCima();
+tiraMouseDeCima();
+
+//  Exercício 7:
+function criaTarefas(tarefa) {
+    let novaTarefa = document.createElement("li");
+    let listaDeTarefas = document.querySelector(".my-tasks");
+    listaDeTarefas.appendChild(novaTarefa);
+    novaTarefa.innerHTML = tarefa;
+}
+criaTarefas("a")
+criaTarefas("b")
+criaTarefas("c")
