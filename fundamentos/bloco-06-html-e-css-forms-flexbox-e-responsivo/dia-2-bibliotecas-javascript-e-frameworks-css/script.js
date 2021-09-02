b6bd9d9b22ba1313f709b2cf5eca49a619864429
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-02 02:04:28 -0300

    finalizando exercicios

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,4 +1,4 @@
-import "./node_modules/just-validate/dist/js/just-validate.js";
+import './node_modules/just-validate/dist/js/just-validate.js';
 
 let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins", "Distrito Federal"];
 
