9c29424b9d8c202ab17ca43dcd44069ced3859eb
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-02 02:15:55 -0300

    arrumando um erro de gitignore

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,12 @@
+function inserirEstados() {
+    let selectState = document.querySelector("#estado");
+    for (i = 0; i < estados.length; i += 1) {
+    let optionState = document.createElement("option");
+    selectState.appendChild(optionState);
+    optionState.innerHTML = estados[i];
+    }
+}
+let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins", "Distrito Federal"];
+
+inserirEstados();
+
