18dcc5a7c658ebe93956ec6406c0b6fea92aca3a
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-06 08:54:34 -0300

    finalizando exercicios de fixação de unit tests

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/parte-3/exercicio-4.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/parte-3/exercicio-4.js
@@ -3,7 +3,7 @@ const assert = require("assert");
 function secondThirdSmallest(array) {
     let results = []
     array.sort(function (x, y) {
-        return x + y;
+        return x - y;
     });
     results = [array[1], array[2]];
     return results;
