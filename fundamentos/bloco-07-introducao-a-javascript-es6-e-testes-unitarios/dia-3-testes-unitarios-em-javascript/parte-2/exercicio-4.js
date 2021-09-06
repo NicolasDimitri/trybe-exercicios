97f80e1c89b63d05574e241a6e9f2f74a8829cd3
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-06 09:02:24 -0300

    ultimo exercicio

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/parte-2/exercicio-4.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/parte-2/exercicio-4.js
@@ -1,30 +1,28 @@
 const assert = require('assert');
 // escreva a função findTheNeedle aqui
 const findTheNeedle = (array, palavraSolicitada) => {
-    for (let posicaoDaPalavra in array) {
-        console.log(posicaoDaPalavra)
+    let resultado = -1;
+    for (const posicaoDaPalavra in array) {
+        if(palavraSolicitada === array[posicaoDaPalavra]) {
+            resultado = posicaoDaPalavra;
+        }
     }
+    return parseInt(resultado);
     }
     
 
 
 let words = ['house', 'train', 'slide', 'needle', 'book'];
-// let expected = 3;
-// let output = findTheNeedle(words, 'needle');
-// assert.strictEqual(output, expected);
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
 
-// words = ['plant', 'shelf', 'arrow', 'bird'];
-// expected = 0;
-// output = findTheNeedle(words, 'plant');
-// assert.strictEqual(output, expected);
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
 
-// words = ['plant', 'shelf', 'arrow', 'bird'];
-// expected = -1;
-// output = findTheNeedle(words, 'plat');
-// assert.strictEqual(output, expected);
-
-// console.log(findTheNeedle(words, "needle"));
-
-for (let posicaoDaPalavra in words) {
-    console.log(posicaoDaPalavra)
-}
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
