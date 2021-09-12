e9f8f1aa1a3d31f13fbe518ebfbddb7ae1a53f57
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-12 08:10:53 -0300

    exercicios parte II

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus/exercicios-parte-I.js
@@ -0,0 +1,60 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = (dragon) =>
+  Math.trunc(Math.random() * (dragon.strength - 15)) + 15;
+
+const warriorDamage = () =>
+  Math.trunc(
+    Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength)
+  ) + warrior.strength;
+
+
+
+
+
+
+
+
+  const mageDamage = () => {
+    let manaCalc = mage.mana;
+    if(mage.mana < 15) {
+      return "Não possui mana suficiente"
+    } else {      
+      manaCalc = mage.mana - 15;
+    }
+    return {
+    dano: Math.floor(Math.random() * ((mage.intelligence * 2) - mage.intelligence)) + mage.intelligence,
+    mana: manaCalc    
+  }};
+  
+
+  const gameActions = {
+    // Crie as HOFs neste objeto.
+    warriorTurn: callBack => {
+      warrior.damage = callBack();
+      dragon.healthPoints -= callBack();
+    }
+  };
+  
+  gameActions.warriorTurn(warriorDamage)
+  console.log(dragon.healthPoints);
