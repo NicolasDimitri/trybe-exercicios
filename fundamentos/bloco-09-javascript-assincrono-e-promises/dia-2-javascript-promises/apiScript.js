3077a8755062eea1f7c14b77086a0585068b814a
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-09-24 05:00:36 -0300

    finalizando exericicos

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-2-javascript-promises/apiScript.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-2-javascript-promises/apiScript.js
@@ -3,14 +3,14 @@ const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
-  const myobj = { 
-      method: 'GET',
-      headers: { 'Accept': 'application/json' }
+  const myobj = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
   };
-  
+
   fetch(API_URL, myobj)
-  .then(response => response.json())
-  .then(data => console.log(data))
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 };
 
 window.onload = () => fetchJoke();
