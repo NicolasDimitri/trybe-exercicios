dc665146071cb0f028e4f1609ada8a61fe053d80
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2021-11-15 08:26:14 -0300

    exercicios de ficação

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/joke-app/src/App.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/joke-app/src/App.js
@@ -1,30 +1,25 @@
 // App.js
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      joke: '',
+      joke: "",
     };
   }
 
   componentDidMount() {
-    const API_URL = 'https://icanhazdadjoke.com/';
-    fetch(API_URL, { headers: { Accept: 'application/json' } })
+    const API_URL = "https://icanhazdadjoke.com/";
+    fetch(API_URL, { headers: { Accept: "application/json" } })
       .then((response) => response.json())
-      .then(({joke}) => this.setState({ joke }));
+      .then(({ joke }) => this.setState({ joke }));
   }
 
   render() {
     const { joke } = this.state;
-
-    return (
-      <div className="App">
-        {joke}
-      </div>
-    );
+    return <div className="App">{joke}</div>;
   }
 }
 
